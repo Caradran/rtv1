af54eabd3546d204c0df26f7eadd7c771113299f
Njiall <mbeilles@student.42.fr>
2018-02-06 14:31:47 +0100

    update header

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: mbeilles <mbeilles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/31 11:08:41 by mbeilles          #+#    #+#             */
-/*   Updated: 2018/01/31 11:49:40 by mbeilles         ###   ########.fr       */
+/*   Updated: 2018/02/06 12:01:33 by mbeilles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define PARSER_H
 
 # include <stdint.h>
+# include "rtv1.h"
 
 /*
 ** =============================================================================
@@ -25,7 +26,7 @@
 ** =============================================================================
 */
 
-typedef enum				s_lexel_state
+typedef enum				e_lexer_state
 {
 	LEXER_STATE_SEPARATOR = 0,
 	LEXER_STATE_PARAMETER_NUMBER,
@@ -66,10 +67,45 @@ typedef struct				s_token
 	t_lexer_state			state;
 }							t_token;
 
+t_token						get_next_token(t_token_info *i);
+
 /*
 ** =============================================================================
 ** 							Parser
 ** =============================================================================
 */
+
+# define PARSER_PATTERN_MAX	6
+# define LEXER_NULL_TOKEN	((t_token){NULL, 0, LEXER_STATE_MAX})
+
+typedef uint32_t			(*t_parser_func)(t_token, t_token_info*, t_env*);
+
+typedef enum				e_parser_state
+{
+	PARSER_VALID = 0,
+	PARSER_ERROR_SYNTAX,
+	PARSER_ERROR_INIT,
+	PARSER_ERROR_TOKEN
+}							t_parser_state;
+
+typedef struct				s_parser_pattern
+{
+	char					*str;
+	uint32_t				len;
+	t_parser_func			func;
+}							t_parser_pattern;
+
+t_token_info				create_info_token(char *path);
+
+uint32_t					parse_color(t_token_info *i, t_col *c);
+uint32_t					parse_vector(t_token_info *i, t_vect *v);
+
+uint32_t					parse_sphere(t_token t, t_token_info *i, t_env *e);
+uint32_t					parse_plane(t_token t, t_token_info *i, t_env *e);
+uint32_t					parse_light(t_token t, t_token_info *i, t_env *e);
+uint32_t					parse_cone(t_token t, t_token_info *i, t_env *e);
+uint32_t					parse_cam(t_token t, t_token_info *i, t_env *e);
+uint32_t					parse_cylinder(t_token t, t_token_info *i
+		, t_env *e);
 
 #endif
